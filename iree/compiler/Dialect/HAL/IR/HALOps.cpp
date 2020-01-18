0412b49cbe57d773d35790be625497169c3582fd
Ben Vanik <ben.vanik@gmail.com>
2020-01-18 12:00:52 -0800

    Unifying fences and semaphores as just timeline semaphores.

--- iree/compiler/Dialect/HAL/IR/HALOps.cpp
+++ iree/compiler/Dialect/HAL/IR/HALOps.cpp
@@ -1463,6 +1463,15 @@ void ExecutableLayoutLookupOp::getAsmResultNames(
 }
 
 //===----------------------------------------------------------------------===//
+// hal.semaphore.create
+//===----------------------------------------------------------------------===//
+
+void SemaphoreCreateOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  setNameFn(result(), "semaphore");
+}
+
+//===----------------------------------------------------------------------===//
 // TableGen definitions (intentionally last)
 //===----------------------------------------------------------------------===//
 
